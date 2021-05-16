17bc32249fe4bdce1466d6cc8e589cf18950892e
qrissy <qris09@gmail.com>
2021-05-16 06:14:40 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/giditourguide/CategoryAdapter.java
@@ -0,0 +1,64 @@
+package com.example.giditourguide;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /** Context of the app */
+    private Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param context is the context of the app
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mContext = context;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new HotelsFragment();
+        } else if (position == 1){
+            return new RestaurantsFragment();
+        } else if (position == 2){
+            return new NightClubsFragment();
+        } else {
+            return new EventsFragment();
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_hotels);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_restaurants);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_night_clubs);
+        } else {
+            return mContext.getString(R.string.category_events);
+        }
+    }
+}
